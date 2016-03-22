a5e304986c0a4d3ceab2e4d8c24e55a855650813
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2016-03-22 12:32:20 -0300

    Working in english

--- yeoman/app/scripts/views/main.js
+++ yeoman/app/scripts/views/main.js
@@ -4,19 +4,38 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'responsivevoice',
   'templates'
-], ($, _, Backbone, JST) => {
+], ($, _, Backbone, rVoice, JST) => {
   "use strict";
 
   class MainView extends Backbone.View {
     constructor() {
-      super({el: '#viewlet-texttospeech'});
+      var options = {};
+      options.el = '#viewlet-texttospeech';
+      options.events = {
+        'click #texttospeech-button': 'play_pause'
+      };
+      super(options);
       this.template = JST['app/scripts/templates/main.ejs'];
+      this.playing = false;
       this.render();
     }
 
     render() {
       this.$el.html(this.template());
+    }
+
+    play_pause() {
+      if (this.playing) {
+        this.playing = false;
+        this.$('#texttospeech-button').attr('value', 'Play');
+        rVoice.cancel();
+      } else {
+        this.playing = true;
+        this.$('#texttospeech-button').attr('value', 'Pause');
+        rVoice.speak($('#content').text());
+      }
     }
   };
 
